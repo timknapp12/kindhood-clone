27de11cee071a20901d4768c53b0e2b5ee3a87e6
timknapp12 <timknapp12@gmail.com>
2018-01-05 15:54:26 -0700

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,14 @@ import React from 'react';
 export default function Navbar() {
 
     return(
-        <div>
-            Navbar
+        <div className='nav'>
+            <ul className='nav_itmes'>
+                <li>SHOP</li>
+                <li>LOOK BOOK</li>
+                <li>ABOUT US</li>
+                <li>CONTACT</li>
+                <li>FAQ</li>
+            </ul>
         </div> 
     )
 }

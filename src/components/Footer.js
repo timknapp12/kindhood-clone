99ff7e14735086372a629c7bc04603af58f0eac6
timknapp12 <timknapp12@gmail.com>
2018-02-15 15:51:57 -0700

    social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import newsletter from '../assets/newsletter_graphic.png';
+import facebook from '../assets/facebook_icon.png';
+import pinterest from '../assets/pinterest_icon.png';
+import insta from '../assets/instagram_icon.png';
 
 
 export default function Footer() {
@@ -19,9 +22,18 @@ export default function Footer() {
                 <span className='footer__final-grid'>© 2018 Kindhood | Keep Kindness Moving</span>
             </div> 
             <div className='footer__second-grid' >
-                <span className=''> Facebook </span>
-                <span className=''> Pinterest </span>
-                <span className=''> Instagram </span>
+                <div className='footer__second-grid-social-and-text' >
+                    <img src={facebook} className='social-icon' alt='facebook-icon'/>
+                    <span className=''> Facebook </span>
+                </div>
+                <div className='footer__second-grid-social-and-text' id='pinterest'>
+                    <img src={pinterest} className='social-icon' alt='pinterest-icon'/>
+                    <span className=''> Pinterest </span>
+                </div>
+                <div className='footer__second-grid-social-and-text'id='insta' >
+                    <img src={insta} className='social-icon'  alt='insta-icon'/>
+                    <span className=''> Instagram </span>
+                </div>
             </div> 
             <div className='footer__newsletter' >
                 <img className='footer__newsletter__image' alt='newsletter' src={newsletter}/>

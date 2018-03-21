0d132a2925065f82c8f7b3a4164c03303d6baf3e
timknapp12 <timknapp12@gmail.com>
2018-03-21 17:19:44 -0600

    retail

--- src/components/Retail.js
+++ src/components/Retail.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 export default function Retail(props) {
- 
-    
-    return(
-        <div>
-            Retail Locations Page
-        </div> 
-    )
+  return (
+    <div className="retail-body">
+      <div className="retail-container">
+        <h3 className="retail-container__heading">KINDHOOD RETAILERS</h3>
+        <p className="retail-container__paragraph">
+            Kindhood’s innovative girls’ and boys’ poncho towels are more than just a comfy, absorbent, and fast-drying towel. These children’s cover-ups are a solution to the frequent complaints of a child who’s “freezing” after swimming or bathing. Our towels also put an end to the fuss and frustration of a child who’s wrapped up tight in a bath towel, unable to move or play. Our girls’ and boys’ poncho towels can easily be slipped over your little one’s head, so they can continue to run, play, and stay warm. As talk about our free-fitting and fashionable children’s cover-ups increases, so do the opportunities for our poncho and hooded towels to be sold in stores. Find out which of your favorite stores we’re partnered with here. 
+        </p>
+
+        <h5 className="retail-container__subheading">HOW TO PARTNER WITH US</h5>
+        <p className="retail-container__paragraph" >
+            We’re always looking for brand representatives and ambassadors. From
+            wholesale to retail, <a>contact us today</a> for more information about how
+            to partner with Kindhood.
+        </p>
+      </div>
+    </div>
+  );
 }

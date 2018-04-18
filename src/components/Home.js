5b2779c6ffd7ef1938eefb3efcd0b603cf3bf246
timknapp12 <timknapp12@gmail.com>
2018-04-18 15:37:39 -0600

    background image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
 import photo1 from '../assets/girl-in-hoodie.jpeg';
+import photo2 from '../assets/mom-pool.jpeg';
+import photo3 from '../assets/kids-beach.jpeg';
+import photo4 from '../assets/side-pool.jpeg';
 
 
 const Home = () => {
     return(
-        <div className='carousel-photo' >
+        <div 
+        // className='carousel-photo' 
+        >
             {/* <img alt='girl in hoodie' src={photo1} /> */}
+                <Carousel>
+                <div>
+                    <img src={photo1} />
+                    {/* <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption> */}
+                </div>
+                <div>
+                    <img src={photo2} />
+                </div>
+                <div>
+                    <img src={photo3} />
+                </div>
+                <div>
+                    <img src={photo4} />
+                </div>
+                </Carousel>;
         </div> 
     )
 }

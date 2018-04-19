2a7ad7716d4885a7e3c77c467eb0f3ac83b181be
timknapp12 <timknapp12@gmail.com>
2018-04-19 13:44:03 -0600

    finally crousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,94 +1,35 @@
 import React, { Component } from "react";
-// import { Carousel } from "react-responsive-carousel";
-// import {
-//   CarouselProvider,
-//   Slider,
-//   Slide,
-//   ButtonBack,
-//   ButtonNext
-// } from "pure-react-carousel";
-// import Carousel from '@brainhubeu/react-carousel';
 import BackgroundSlideshow from "react-background-slideshow";
-import Slider from "react-slick";
-import Coverflow from "react-coverflow";
+import Carousel from 'nuka-carousel';
 import photo1 from "../assets/girl-in-hoodie.jpeg";
 import photo2 from "../assets/mom-pool.jpeg";
 import photo3 from "../assets/kids-beach.jpeg";
 import photo4 from "../assets/side-pool.jpeg";
 
 export default class Home extends Component {
+
+    state = {
+        photos: [photo1, photo2, photo3, photo4]
+    }
+
+        
+
   render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
+    const photos = this.state.photos.map((photo, i) => {
+        return(
+            <div key={i} >
+                <img src={photo} />
+            </div> 
+        )
+    })
 
     return (
       <div className="home-body">
-        <Slider {...settings} className="slick">
-          <div>
-            <img src={photo1} />
-          </div>
-          <div>
-            <img src={photo2} />
-          </div>
-          <div>
-            <img src={photo3} />
-          </div>
-          <div>
-            <img src={photo4} />
-          </div>
-          {/* <div>
-          <h3>5</h3>
-        </div> */}
-        </Slider>
-        {/* <img alt='girl in hoodie' src={photo1} /> */}
-        {/* <Carousel>
-            <div>
-            <img src={photo1} /> */}
-        {/* <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption> */}
-        {/* </div>
-            <div>
-            <img src={photo2} />
-            </div>
-            <div>
-            <img src={photo3} />
-            </div>
-            <div>
-            <img src={photo4} />
-            </div>
-        </Carousel>; */}
+        <Carousel className="slick">
+            {photos}
+        </Carousel>
 
-        {/* <CarouselProvider
-            naturalSlideWidth={700}
-            naturalSlideHeight={0}
-            totalSlides={3}
-            className=''
-        >
-            <Slider className="carousel-provider" >
-            <Slide index={0}><img hasMasterSpinner={true} src={photo1} /></Slide>
-            <Slide index={1}><img hasMasterSpinner={true} src={photo2} /></Slide>
-            <Slide index={2}><img hasMasterSpinner={true} src={photo3} /></Slide>
-            </Slider>
-            <ButtonBack>Back</ButtonBack>
-            <ButtonNext>Next</ButtonNext>
-        </CarouselProvider> */}
-
-        {/* <Carousel className='react-carousel' >
-            <div className='react-carousel__inner' >
-                <img src={photo1} />
-                <img src={photo2} />
-                <img src={photo3} />
-            </div> 
-        </Carousel> */}
-
-        {/* this one works great for automatic transitions between backgound photos */}
+        {/* this one works great for automatic transitions between backgound photos from 'react-background-slideshow' */}
         {/* <div className='background-slideshow'>
             <BackgroundSlideshow images={[ photo1, photo2, photo3, photo4 ]} />
         </div>*/}

8561b9bc8f74c0b48842c8e848f0180147585950
timknapp12 <timknapp12@gmail.com>
2018-04-30 13:52:02 -0600

    nodemailer

--- server/index.js
+++ server/index.js
@@ -4,12 +4,20 @@ const bodyParser = require('body-parser');
 const massive = require('massive');
 const session = require('express-session');
 const cors = require('cors');
+const exphbs = require('express-handlebars');
+const nodemailer = require('nodemailer');
 
 const port = 4000
 
 const app = express();
+app.use( express.static( `${__dirname}/../build` ) );
+
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
 
 app.use(
@@ -45,5 +53,13 @@ app.get('/api/locations', (req, res, next) => {
     db.locations().then(locations => res.status(200).send(locations))
 })
 
+app.post('/send', (req, res) => {
+    console.log(req.body);
+})
+
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 app.listen(port, () => console.log(`listening on port ${port}`));
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import newsletter from '../assets/newsletter_graphic.png';
 import facebook from '../assets/facebook_icon.png';
 import pinterest from '../assets/pinterest_icon.png';
@@ -10,61 +10,89 @@ import smallfry from '../assets/SMALL-FRY-LOGO-04_compact.png';
 import nursery from '../assets/DestinationNurserylogo_compact.png';
 import babiekins from '../assets/babiekins2_compact.png';
 import tatler from '../assets/tatler2_compact.png';
+import axios from 'axios';
 
+export default class Footer extends Component {
 
-export default function Footer() {
+    handleSubmit(e){
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+        axios({
+            method: "POST", 
+            url:"http://localhost:3002/send", 
+            data: {
+                name: name,   
+                email: email,  
+                messsage: message
+            }
+        }).then((response)=>{
+            if (response.data.msg === 'success'){
+                alert("Message Sent."); 
+                this.resetForm()
+            }else if(response.data.msg === 'fail'){
+                alert("Message failed to send.")
+            }
+        })
+    }
 
+    render() {
 
-    return(
-        <div className='footer' >
-            <div className='footer__as-seen-in-container' >
-                <div className='footer__as-seen-in-container--heading' >AS SEEN IN</div> 
-                <div className='footer__as-seen-in-container--images' >
-                    <img  className='popsugar' alt='popsugar' id='pop' src={popsugar} />
-                    <img  className='popsugar' alt='mini' src={mini} />
-                    <img  className='popsugar' alt='vogue' id='pop' src={vogue} />
-                    <img  className='popsugar' alt='smallfry' id='pop' src={smallfry} />
-                    <img  className='popsugar' alt='nursery' id='pop' src={nursery} />
-                    <img  className='popsugar' alt='babiekins' src={babiekins} />
-                    <img  className='popsugar' alt='tatler' src={tatler} />
+        return(
+            <div className='footer' >
+                <div className='footer__as-seen-in-container' >
+                    <div className='footer__as-seen-in-container--heading' >AS SEEN IN</div> 
+                    <div className='footer__as-seen-in-container--images' >
+                        <img  className='popsugar' alt='popsugar' id='pop' src={popsugar} />
+                        <img  className='popsugar' alt='mini' src={mini} />
+                        <img  className='popsugar' alt='vogue' id='pop' src={vogue} />
+                        <img  className='popsugar' alt='smallfry' id='pop' src={smallfry} />
+                        <img  className='popsugar' alt='nursery' id='pop' src={nursery} />
+                        <img  className='popsugar' alt='babiekins' src={babiekins} />
+                        <img  className='popsugar' alt='tatler' src={tatler} />
+                    </div> 
+                </div> 
+                <div className='footer__grid' >
+                    <div className='footer__first-grid' >
+                        <span className=''> Contact Us </span>
+                        <span className=''> Sizing Guide </span>
+                        <span className=''> Care Instructions </span>
+                        <span className=''> Order Status </span>
+                        <span className=''> Exchanges & Returns </span>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <span className='footer__final-grid'>© 2018 Kindhood | Keep Kindness Moving</span>
+                    </div> 
+                    <div className='footer__second-grid' >
+                        <div className='footer__second-grid-social-and-text' >
+                            <img src={facebook} className='social-icon' alt='facebook-icon'/>
+                            <span className=''> Facebook </span>
+                        </div>
+                        <div className='footer__second-grid-social-and-text' id='pinterest'>
+                            <img src={pinterest} className='social-icon' alt='pinterest-icon'/>
+                            <span className=''> Pinterest </span>
+                        </div>
+                        <div className='footer__second-grid-social-and-text'id='insta' >
+                            <img src={insta} className='social-icon'  alt='insta-icon'/>
+                            <span className=''> Instagram </span>
+                        </div>
+                    </div> 
+                    <div className='footer__newsletter' >
+                        <img className='footer__newsletter__image' alt='newsletter' src={newsletter}/>
+                        <div className='footer__newletter__input-and-button-div' > 
+                            <form id='contact-form' onSubmit={() => this.handleSubmit()} method='POST' > 
+                                <input  type='email' name='email' action='send' className='footer__newsletter__input' />
+                                <button type='submit' className='footer__newsletter__button'>SUBSCRIBE</button>
+                            </form>
+                        </div>
+                    </div> 
+                    <div>
+                    </div> 
                 </div> 
             </div> 
-            <div className='footer__grid' >
-                <div className='footer__first-grid' >
-                    <span className=''> Contact Us </span>
-                    <span className=''> Sizing Guide </span>
-                    <span className=''> Care Instructions </span>
-                    <span className=''> Order Status </span>
-                    <span className=''> Exchanges & Returns </span>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <span className='footer__final-grid'>© 2018 Kindhood | Keep Kindness Moving</span>
-                </div> 
-                <div className='footer__second-grid' >
-                    <div className='footer__second-grid-social-and-text' >
-                        <img src={facebook} className='social-icon' alt='facebook-icon'/>
-                        <span className=''> Facebook </span>
-                    </div>
-                    <div className='footer__second-grid-social-and-text' id='pinterest'>
-                        <img src={pinterest} className='social-icon' alt='pinterest-icon'/>
-                        <span className=''> Pinterest </span>
-                    </div>
-                    <div className='footer__second-grid-social-and-text'id='insta' >
-                        <img src={insta} className='social-icon'  alt='insta-icon'/>
-                        <span className=''> Instagram </span>
-                    </div>
-                </div> 
-                <div className='footer__newsletter' >
-                    <img className='footer__newsletter__image' alt='newsletter' src={newsletter}/>
-                    <div className='footer__newletter__input-and-button-div' > 
-                        <input className='footer__newsletter__input' />
-                        <button type='text' className='footer__newsletter__button'>SUBSCRIBE</button>
-                    </div>
-                </div> 
-                <div>
-                </div> 
-            </div> 
-        </div> 
-    )
+        )
+    }
+
 }

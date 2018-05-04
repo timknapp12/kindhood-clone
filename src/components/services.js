cf4117b09cab477ebd2d95299fa6f6418d16d24a
timknapp12 <timknapp12@gmail.com>
2018-05-04 08:44:34 -0600

    async function

--- src/components/services.js
+++ src/components/services.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
-export const displayProducts = () => axios.get('/api/products')
-.then(res => res.data)
-.catch((res) => res.status(500).send());
+// export const displayProducts = () => axios.get('/api/products')
+// .then(res => res.data)
+// .catch((res) => res.status(500).send());
+
+export async function displayProducts() {
+    const res = await axios.get('/api/products');
+    return res.data;
+}
 
 export const displayProduct = () => axios.get('/api/products/:id').then(res => res.data)
